2bf77e22d8d57ff6e801047e88492976676f7977
Tharnslair <jsmith33@ittesi.com>
2014-09-11 18:12:32 -0400

    will need to upgraded to express 4 so I can use morgan

--- MEAN/TharnVision/server.js
+++ MEAN/TharnVision/server.js
@@ -1,5 +1,7 @@
 // Require statements up here
 var express = require('express');
+	stylus = require('stylus');
+	logger = require('morgan');     
 
 // setting environment
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -7,15 +9,33 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // instance of express app
 var app = express();
 
+// use stylus
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
+
 // app configuration
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+// express logging
+app.use(('dev'));
+// adding stylus middleware
+app.use(stylus.middleware(
+	{
+		src: __dirname + '/public',
+		compile: compile
+	}
+));
 
+// express static
+app.use(express.static(__dirname + '/public'));
 
 // routes 
 app.get('*', function(req, res) { // match all routes
 	res.render('index');
 });
+
+
 
 // server logging sort of kinda...
 var port = 1974;
