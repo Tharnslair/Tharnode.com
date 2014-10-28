500234c30eaaecdb19503329ad667a25ab448ee0
Tharnslair <jsmith33@ittesi.com>
2014-10-28 15:17:01 -0400

    done with mongoose model endpoint

--- TharnSocial/server.js
+++ TharnSocial/server.js
@@ -6,22 +6,27 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
-// routes
-app.get('/api/posts', function(req, res) {
-    res.json([
-        {
-            username: 'Tharnid',
-            body: 'Node Roxxors!!!'
-        }
-    ])
+// GET request
+app.get('/api/posts', function(req, res, next) {
+    Post.find(function(err, posts) {
+        if (err) { return next(err) }
+        res.json(posts);
+    })
 })
 
-app.post('api/posts', function(req, res) {
-    console.log('post received!!!');
-    console.log(req.body.username);
-    console.log('req.body.body');
-    res.send(201);
+// routes
+app.post('/api/posts', function (req, res, next) {
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    })
+    post.save(function (err, post) {
+        if (err) { return next(err) }
+        res.json(201, post)
+    })
 })
+
+
 
 // Server listening
 app.listen(1966, function () {
